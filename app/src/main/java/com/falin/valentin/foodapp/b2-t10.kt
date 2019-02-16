bc02e96a74d25af3ce51f87b339f61fc993e04c4
Golgoroth22 <falinv22@gmail.com>
2019-02-16 23:46:39 +0700

    First result for b2-t10.

---
+++ app/src/main/java/com/falin/valentin/foodapp/b2-t10.kt
@@ -0,0 +1,31 @@
+package com.falin.valentin.foodapp
+
+fun main() {
+    val animals = listOf(
+        Animal.Cat(name = "Anna"),
+        Animal.Dog(name = "Mike"),
+        Animal.Mouse(name = "Mickey")
+    )
+
+    animals.animalListToString(ShowBy.NOISE, separator = " ").also(::println)
+}
+
+fun List<Animal>.animalListToString(showBy: ShowBy? = null, separator: String = ""): String {
+    val result = StringBuilder("")
+    for (i in 0 until this.size) {
+        when (showBy) {
+            ShowBy.NAME -> result.append("${this[i].name}$separator")
+            ShowBy.AGE -> result.append("${this[i].age}$separator")
+            ShowBy.NOISE -> result.append("${this[i].noise}$separator")
+            null -> result.append("${this[i]}$separator")
+        }
+    }
+    return result.toString()
+}
+
+enum class ShowBy {
+    NAME,
+    AGE,
+    NOISE
+}
+
