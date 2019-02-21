2bbbe9a7534f6f639b6fd69544d0628b7b848d60
Golgoroth22 <falinv22@gmail.com>
2019-02-21 23:39:54 +0700

    Finish first variant for b2-t12.

--- app/src/main/java/com/falin/valentin/foodapp/b2-t12.kt
+++ app/src/main/java/com/falin/valentin/foodapp/b2-t12.kt
@@ -28,6 +28,72 @@ enum class Gender(s: String) {
     }
 }
 
+class House {
+    private val persons: MutableList<Person> = mutableListOf()
+    private val children: MutableList<Person> = mutableListOf()
+    private val adultWomen: MutableList<Person> = mutableListOf()
+    private val adultMen: MutableList<Person> = mutableListOf()
+
+    val getAdults: List<Person>
+        get() {
+            val adults: MutableList<Person> = mutableListOf()
+            for (p in persons) {
+                if (p.age ?: 0 > 18) {
+                    adults.add(p)
+                }
+            }
+            return adults
+        }
+
+    init {
+        children.add(Person("Anna", 2, Gender.FEMALE))
+        adultMen.add(Person("Mike", 45, Gender.MALE))
+        adultWomen.add(Person("Maria", 45, Gender.FEMALE))
+        children.add(Person("Boris", 15, Gender.MALE))
+        adultMen.add(Person("Jordan", 21, Gender.MALE))
+
+        persons.addAll(children)
+        persons.addAll(adultWomen)
+        persons.addAll(adultMen)
+    }
+
+    fun addPerson(p: Person) {
+        if (p.age ?: 0 > 18) {
+            if (p.gender === Gender.MALE) {
+                adultMen.add(p)
+            } else {
+                adultWomen.add(p)
+            }
+        } else {
+            children.add(p)
+        }
+
+        persons.add(p)
+    }
+
+    fun sayHelloToPerson(p: Person?) {
+        var greet: String? = null
+
+        if (p != null) {
+            greet = when {
+                p.age ?: 0 < 14 -> "Hello, little " + p.name!!
+                p.age ?: 0 in 14..17 -> when (p.gender) {
+                    Gender.MALE -> "Hello boy!"
+                    Gender.FEMALE -> "Hello girl!"
+                }
+                else -> when (p.gender) {
+                    Gender.MALE -> "Hello  man!"
+                    Gender.FEMALE -> "Hello  woman!"
+                }
+            }
+        }
+
+        if (greet != null) {
+            print(greet)
+        }
+    }
+}
+
 open class Person(
     val name: String?,
     var age: Int? = 0,
