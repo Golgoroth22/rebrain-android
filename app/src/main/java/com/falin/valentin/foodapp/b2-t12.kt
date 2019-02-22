10f92614cd2770cfef671d7bc71fa28b7211d1ca
Golgoroth22 <falinv22@gmail.com>
2019-02-23 00:46:51 +0700

    Fix some mistakes for b2-t12.

--- app/src/main/java/com/falin/valentin/foodapp/b2-t12.kt
+++ app/src/main/java/com/falin/valentin/foodapp/b2-t12.kt
@@ -2,13 +2,15 @@ package com.falin.valentin.foodapp
 
 fun main() {
 //    val map = mapOf(
-//        "age" to "25",
+//        "age" to "25a",
 //        "name" to "Bob",
 //        "gender" to "m"
 //    )
 //
 //    val p = Person.from(map)
 //    print(p.age)
+
+    House().getAdults.also { it.forEach { println(it.name) } }
 }
 
 enum class Gender(s: String) {
@@ -29,36 +31,31 @@ enum class Gender(s: String) {
 }
 
 class House {
-    private val persons: MutableList<Person> = mutableListOf()
-    private val children: MutableList<Person> = mutableListOf()
-    private val adultWomen: MutableList<Person> = mutableListOf()
-    private val adultMen: MutableList<Person> = mutableListOf()
+    private val children = mutableListOf(
+        Person("Anna", 2, Gender.FEMALE),
+        Person("Boris", 15, Gender.MALE)
+    )
+    private val adultWomen = mutableListOf(Person("Maria", 45, Gender.FEMALE))
+    private val adultMen = mutableListOf(
+        Person("Mike", 45, Gender.MALE),
+        Person("Jordan", 21, Gender.MALE)
+    )
+
+    private val persons = mutableListOf<Person>()
+
+    init {
+        persons.addAll(children)
+        persons.addAll(adultMen)
+        persons.addAll(adultWomen)
+    }
 
     val getAdults: List<Person>
         get() {
-            val adults: MutableList<Person> = mutableListOf()
-            for (p in persons) {
-                if (p.age ?: 0 > 18) {
-                    adults.add(p)
-                }
-            }
-            return adults
+            return persons.filter { p -> p.age > 18 }
         }
 
-    init {
-        children.add(Person("Anna", 2, Gender.FEMALE))
-        adultMen.add(Person("Mike", 45, Gender.MALE))
-        adultWomen.add(Person("Maria", 45, Gender.FEMALE))
-        children.add(Person("Boris", 15, Gender.MALE))
-        adultMen.add(Person("Jordan", 21, Gender.MALE))
-
-        persons.addAll(children)
-        persons.addAll(adultWomen)
-        persons.addAll(adultMen)
-    }
-
     fun addPerson(p: Person) {
-        if (p.age ?: 0 > 18) {
+        if (p.age > 18) {
             if (p.gender === Gender.MALE) {
                 adultMen.add(p)
             } else {
@@ -72,36 +69,37 @@ class House {
     }
 
     fun sayHelloToPerson(p: Person?) {
-        var greet: String? = null
 
-        if (p != null) {
-            greet = when {
-                p.age ?: 0 < 14 -> "Hello, little " + p.name!!
-                p.age ?: 0 in 14..17 -> when (p.gender) {
-                    Gender.MALE -> "Hello boy!"
-                    Gender.FEMALE -> "Hello girl!"
+        p?.let {
+            print(
+                when {
+                    p.age < 14 -> "Hello, little " + p.name
+                    p.age in 14..17 -> when (p.gender) {
+                        Gender.MALE -> "Hello boy!"
+                        Gender.FEMALE -> "Hello girl!"
+                    }
+                    else -> when (p.gender) {
+                        Gender.MALE -> "Hello  man!"
+                        Gender.FEMALE -> "Hello  woman!"
+                    }
                 }
-                else -> when (p.gender) {
-                    Gender.MALE -> "Hello  man!"
-                    Gender.FEMALE -> "Hello  woman!"
-                }
-            }
-        }
-
-        if (greet != null) {
-            print(greet)
+            )
         }
     }
 }
 
 open class Person(
-    val name: String?,
-    var age: Int? = 0,
+    val name: String,
+    var age: Int,
     val gender: Gender
 ) {
     companion object {
         fun from(personMap: Map<String, String>): Person {
-            return Person(personMap["name"], personMap["age"]?.toIntOrNull(), Gender.getById(personMap["gender"]))
+            return Person(
+                personMap["name"].toString(),
+                personMap["age"]?.toIntOrNull() ?: 0,
+                Gender.getById(personMap["gender"])
+            )
         }
     }
 }
