efd6f39d336bb7b8999c91e4e82a53d57ad90973
Golgoroth22 <falinv22@gmail.com>
2019-03-06 23:57:05 +0700

    Start work on IntroActivity

--- app/src/main/java/com/falin/valentin/foodapp/screen/intro/IntroActivity.kt
+++ app/src/main/java/com/falin/valentin/foodapp/screen/intro/IntroActivity.kt
@@ -1,19 +1,20 @@
-package com.falin.valentin.foodapp
+package com.falin.valentin.foodapp.screen.intro
 
 import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.falin.valentin.foodapp.R
 
-class MainActivity : AppCompatActivity() {
+class IntroActivity : AppCompatActivity() {
     companion object {
         fun start(context: Context) {
-            context.startActivity(Intent(context, MainActivity::class.java))
+            context.startActivity(Intent(context, IntroActivity::class.java))
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_intro)
     }
 }
--- app/src/main/java/com/falin/valentin/foodapp/screen/splash/SplashActivity.kt
+++ app/src/main/java/com/falin/valentin/foodapp/screen/splash/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.falin.valentin.foodapp.MainActivity
 import com.falin.valentin.foodapp.R
+import com.falin.valentin.foodapp.screen.intro.IntroActivity
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
 
@@ -18,11 +19,11 @@ class SplashActivity : AppCompatActivity(), CoroutineScope {
         waitABitAndGoNext()
     }
 
-    private fun getSplashScreenDuration() = 5000L
+    private fun getSplashScreenDuration() = 500L
 
     private fun waitABitAndGoNext() = launch {
         delay(getSplashScreenDuration())
-        MainActivity.start(applicationContext)
+        IntroActivity.start(applicationContext)
         finish()
     }
 }
