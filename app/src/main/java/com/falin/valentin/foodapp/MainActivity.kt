db298db742d48bc40ecbfec0baf9ff90c742c0d8
Golgoroth22 <falinv22@gmail.com>
2019-03-06 23:12:26 +0700

    Hide action bar

--- app/src/main/java/com/falin/valentin/foodapp/MainActivity.kt
+++ app/src/main/java/com/falin/valentin/foodapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     companion object {
@@ -15,5 +16,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
     }
 }
