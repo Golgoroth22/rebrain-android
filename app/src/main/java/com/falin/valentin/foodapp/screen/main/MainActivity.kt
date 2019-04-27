add2934d639638b1c6375a3510aeebb45157f808
Golgoroth22 <falinv22@gmail.com>
2019-04-27 23:19:45 +0700

    Work on task b3-t11. Change work with fragments to Attach/Detach from Replace

--- app/src/main/java/com/falin/valentin/foodapp/screen/main/MainActivity.kt
+++ app/src/main/java/com/falin/valentin/foodapp/screen/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.falin.valentin.foodapp.screen.main
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
 import com.falin.valentin.foodapp.R
 import com.falin.valentin.foodapp.screen.BaseActivity
 import com.falin.valentin.foodapp.screen.BaseFragment
@@ -24,20 +25,33 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        loadSomeFragment(MainTabFragment.newInstance())
+        val mainTabFragment = MainTabFragment.newInstance()
+        val favoriteTabFragment = FavoriteTabFragment.newInstance()
+        val transaction = supportFragmentManager.beginTransaction()
+            .add(R.id.main_activity_frame_layout, favoriteTabFragment)
+            .detach(favoriteTabFragment)
+            .add(R.id.main_activity_frame_layout, mainTabFragment)
+            .commit()
         main_activity_custom_bottom_bar.setOnCustomClickListener(object : onClickCustomListener {
             override fun onClick(tabType: MainTabType) {
                 when (tabType) {
-                    MainTabType.MAIN -> loadSomeFragment(MainTabFragment.newInstance())
-                    MainTabType.FAVORITE -> loadSomeFragment(FavoriteTabFragment.newInstance())
+                    MainTabType.MAIN -> {
+                        attachNewFragment(favoriteTabFragment, mainTabFragment)
+                    }
+                    MainTabType.FAVORITE -> {
+                        attachNewFragment(mainTabFragment, favoriteTabFragment)
+                    }
                 }
             }
         })
     }
 
-    private fun loadSomeFragment(fragment: BaseFragment) {
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.main_activity_frame_layout, fragment)
-        transaction.commit()
+    private fun attachNewFragment(oldFragment: BaseFragment, newFragment: BaseFragment) {
+        if (!oldFragment.isDetached) {
+            val transaction = supportFragmentManager.beginTransaction()
+                .detach(oldFragment)
+                .attach(newFragment)
+                .commit()
+        }
     }
 }
