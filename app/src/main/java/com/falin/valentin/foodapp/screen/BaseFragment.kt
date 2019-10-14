135dd1d85e49ae367430c9c1c2ea642b00345238
Golgoroth22 <falinv22@gmail.com>
2019-10-14 12:46:19 +0700

    [ADD] Work on b5-t02. Some fixes v.1

--- app/src/main/java/com/falin/valentin/foodapp/screen/BaseFragment.kt
+++ app/src/main/java/com/falin/valentin/foodapp/screen/BaseFragment.kt
@@ -1,13 +1,7 @@
 package com.falin.valentin.foodapp.screen
 
-import android.content.Context
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.falin.valentin.foodapp.utils.Logger
-import timber.log.Timber
 
 /**
  * Parent class-fragment with Timber logging in lifecycle methods.
@@ -16,5 +10,6 @@ import timber.log.Timber
 abstract class BaseFragment : Fragment() {
 
     abstract val owner: Logger.Owner
+
     private val logger = Logger(lifecycle, owner)
 }
