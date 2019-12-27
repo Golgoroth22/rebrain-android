c6009b1d2f62fd6446d6dfbc7f2b64c8c4f32e03
Golgoroth22 <falinv22@gmail.com>
2019-12-27 15:52:51 +0700

    [UPD] Work on b5-t11. 11. Dagger. Scope v.1

--- app/src/main/java/com/falin/valentin/foodapp/di/scope/PerApplication.kt
+++ app/src/main/java/com/falin/valentin/foodapp/di/scope/PerApplication.kt
@@ -2,6 +2,10 @@ package com.falin.valentin.foodapp.di.scope
 
 import javax.inject.Scope
 
+/**
+ * Dagger2 [Scope] annotation class for all application.
+ *
+ */
 @Scope
 @Retention
 annotation class PerApplication
--- app/src/main/java/com/falin/valentin/foodapp/di/scope/PerScreen.kt
+++ app/src/main/java/com/falin/valentin/foodapp/di/scope/PerScreen.kt
@@ -2,6 +2,10 @@ package com.falin.valentin.foodapp.di.scope
 
 import javax.inject.Scope
 
+/**
+ * Dagger2 [Scope] annotation class for screens.
+ *
+ */
 @Scope
 @Retention
 annotation class PerScreen
