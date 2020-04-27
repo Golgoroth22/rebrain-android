40082a550862b89c6e23d150c2801258d4062ca5
Golgoroth22 <falinv22@gmail.com>
2020-04-27 13:10:18 +0700

    [DEL] Remove redundant entity

--- app/src/main/java/com/falin/valentin/foodapp/domain/LoginUiResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.falin.valentin.foodapp.domain
-
-import com.falin.valentin.foodapp.network.retrofit.pojo.login.UserResponse
-
-data class LoginUiResponse(
-    val data: UserResponse? = null,
-    val message: String
-)
