e789fae4b635b39edef134fa2fe720902036bb2f
Golgoroth22 <falinv22@gmail.com>
2020-06-17 18:27:12 +0700

    [UPD] Work on b6-t07. Исправил передачу объекта во фрагмент

--- app/src/main/java/com/falin/valentin/foodapp/screen/main/carousel/CarouselFragment.kt
+++ app/src/main/java/com/falin/valentin/foodapp/screen/main/carousel/CarouselFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import com.bumptech.glide.Glide
 
 import com.falin.valentin.foodapp.R
@@ -17,29 +18,31 @@ import kotlinx.android.synthetic.main.fragment_carousel.view.carousel_image_view
  * [Fragment] subclass to work with CarouselFragment and showing it.
  *
  */
-class CarouselFragment() : BaseFragment() {
+class CarouselFragment : BaseFragment() {
     override val owner: Logger.Owner
         get() = Logger.Owner.CAROUSEL_FRAGMENT
-
-    private lateinit var imageUri: Uri
-
-    constructor(mImageUri: Uri) : this() {
-        imageUri = mImageUri
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_carousel, container, false)
-        Glide.with(context!!)
-            .load(imageUri)
-            .thumbnail(0.25f)
-            .into(rootView.carousel_image_view)
+        arguments?.getString(IMG_URI)?.let {
+            Glide.with(context!!)
+                .load(it)
+                .thumbnail(0.25f)
+                .into(rootView.carousel_image_view)
+        }
         return rootView
     }
 
     companion object {
-        fun newInstance(imageUri: Uri) = CarouselFragment(imageUri)
+        fun newInstance(uri: Uri): CarouselFragment {
+            val fragment = CarouselFragment()
+            fragment.arguments = bundleOf(IMG_URI to uri.toString())
+            return fragment
+        }
+
+        private const val IMG_URI = "image_uri"
     }
 }
